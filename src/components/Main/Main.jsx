f04873179ddaddeec23817bb372e67be3573f743
shamo-9118 <won9118391183@gmail.com>
2021-12-20 15:38:18 +0900

    rifakuta

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,17 +4,6 @@ import classes from "./Main.module.css";
 import { useEffect } from "react";
 
 export function Main(props) {
-  useEffect(() => {
-    console.log("マウント");
-    document.body.style.backgroundColor = "lightblue";
-    /* ↑マウント時の処理を書く */
-    return () => {
-      console.log("アンマウント");
-      document.body.style.backgroundColor = "";
-      /* アンマウント時の処理を書く */
-    };
-  }, []);
-  /* useEffectでDOM要素に直接直接干渉することは控える */
    return (
     <main className={classes.main}>
       <Headline page={props.page}>
